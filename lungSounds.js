acb1a4b24ae53580572c401b425100a2650d0981
Erol Cimen <erol.a.cimen@gmail.com>
2019-08-24 16:39:40 +1000

    Added bgColor = "yellow" to clicked answer

--- lungSounds.js
+++ lungSounds.js
@@ -7,12 +7,13 @@ function changeAnswers (element) {
                 `
                  <span id = "examDescription"><i>You start tapping the patient.</i> You find:</span>
                  <ol id = "answers1">
-                    <li class = "multiChoice">Hyper-resonance to percussion</li>
-                    <li class = "multiChoice">Resonant to percussion</li>
-                    <li class = "multiChoice">Dull to percussion</li>
-                    <li class = "multiChoice">Stony dullness</li>
+                    <li id = "hyperresonant" class = "multiChoice">Hyper-resonance to percussion</li>
+                    <li id = "resonant" class = "multiChoice">Resonant to percussion</li>
+                    <li id = "dull" class = "multiChoice">Dull to percussion</li>
+                    <li id = "stony" class = "multiChoice">Stony dullness</li>
                  </ol>
                  `;
+            addClickChange();
             break;
         case "auscultation":
             answerBox.innerHTML =
@@ -22,32 +23,34 @@ function changeAnswers (element) {
                      <span id = "answers2sub">
                          <ol>
                          <h4>Breath sounds</h4>
-                            <li class = "multiChoice">Vesicular breath sounds</li>
-                            <li class = "multiChoice">Bronchial breath sounds</li>
+                            <li id = "vesicular" class = "multiChoice">Vesicular breath sounds</li>
+                            <li id = "bronchial" class = "multiChoice">Bronchial breath sounds</li>
                          </ol>
                      </span>
                      <span id = "answers2sub2">
                          <ol>
                          <h4>Adventitious sounds</h4>
-                            <li class = "multiChoice">No adventitious sounds</li>
-                            <li class = "multiChoice">Wheeze</li>
-                            <li class = "multiChoice">Stridor</li>
-                            <li class = "multiChoice">Crackles</li>
+                            <li id = "noAdded" class = "multiChoice">No adventitious sounds</li>
+                            <li id = "wheeze" class = "multiChoice">Wheeze</li>
+                            <li id = "stridor" class = "multiChoice">Stridor</li>
+                            <li id = "crackles" class = "multiChoice">Crackles</li>
                          </ol>
                      </span>
                  </span>
                 `;
+            addClickChange();
             break;
         case "resonance":
             answerBox.innerHTML =
                 `
                 <span id = "examDescription"><i>You ask the patient to say <b id = "what">'toy boat'</b> while you listen to their chest.</i> You hear: </span>
                  <ol id = "answers1">
-                    <li class = "multiChoice">Increased vocal resonance</li>
-                    <li class = "multiChoice">Normal vocal resonance</li>
-                    <li class = "multiChoice">Reduced vocal resonance</li>
+                    <li id = "increasedVR" class = "multiChoice">Increased vocal resonance</li>
+                    <li id = "normalVR" class = "multiChoice">Normal vocal resonance</li>
+                    <li id = "reducedVR" class = "multiChoice">Reduced vocal resonance</li>
                  </ol>
                 `;
+            addClickChange();
             changeWhat();
             break;
     }
@@ -58,5 +61,25 @@ function changeWhat() {
     randomWhat = Math.floor(Math.random() * whatOptions.length);
     document.getElementById("what").innerHTML = "'" + whatOptions[randomWhat] + "'";
 }
+
+//executed every time you click a multiple choice answer
+function highlightThis () {
+    //alert(this.id);
+    multiChoiceAnswers = document.getElementsByClassName("multiChoice");
+    for (var i = 0; i < multiChoiceAnswers.length; i++) {
+        multiChoiceAnswers[i].style.backgroundColor = "beige";
+    }
+    this.style.backgroundColor = "yellow";
+}
+
+let multiChoiceAnswers;
+function addClickChange () {
+    multiChoiceAnswers = document.getElementsByClassName("multiChoice");
+    for (var i = 0; i < multiChoiceAnswers.length; i++) {
+        multiChoiceAnswers[i].addEventListener("click", highlightThis);
+    }
+}
+
+
 
 // make an object constructor function that creates the "answer object"? or separate objects for every case? or... both?
